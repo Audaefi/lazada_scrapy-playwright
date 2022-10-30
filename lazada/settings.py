2dc43cfb40289b42ecfc1b588dac57d34c3f71e4
KJ.O <nomeansno@Live.ca>
2022-10-30 16:25:18 +0900

    changes

--- lazada/settings.py
+++ lazada/settings.py
@@ -1,23 +1,18 @@
-# Scrapy settings for lazada project
-#
-# For simplicity, this file contains only settings considered important or
-# commonly used. You can find more settings consulting the documentation:
-#
-#     https://docs.scrapy.org/en/latest/topics/settings.html
-#     https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-#     https://docs.scrapy.org/en/latest/topics/spider-middleware.html
-
-BOT_NAME = 'lazada'
+OT_NAME = 'lazada'
 
 SPIDER_MODULES = ['lazada.spiders']
 NEWSPIDER_MODULE = 'lazada.spiders'
 
 ROBOTSTXT_OBEY = False
-USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'
 
 #PLAYWRIGHT_LAUNCH_OPTIONS = {"headless": False}
 
-AUTOTHROTTLE_ENABLED = False
+FEEDS = {"s3://rdata-storage/%(name)s/%(name)s_%(time)s.csv": {"format": "csv"}}
+
+AWS_ACCESS_KEY_ID = ''
+AWS_SECRET_ACCESS_KEY = ''
+
+AUTOTHROTTLE_ENABLED = True
 AUTOTHROTTLE_START_DELAY = 1
 AUTOTHROTTLE_MAX_DELAY = 3
 
