a132627f5ae92dd765cfa526d9a5abe031ac9abb
KJ.O <nomeansno@Live.ca>
2022-10-30 14:01:09 +0900

    changes

--- lazada/settings.py
+++ lazada/settings.py
@@ -13,10 +13,11 @@ SPIDER_MODULES = ['lazada.spiders']
 NEWSPIDER_MODULE = 'lazada.spiders'
 
 ROBOTSTXT_OBEY = False
+USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'
 
 #PLAYWRIGHT_LAUNCH_OPTIONS = {"headless": False}
 
-AUTOTHROTTLE_ENABLED = True
+AUTOTHROTTLE_ENABLED = False
 AUTOTHROTTLE_START_DELAY = 1
 AUTOTHROTTLE_MAX_DELAY = 3
 
